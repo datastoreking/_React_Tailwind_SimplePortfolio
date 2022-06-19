84f3213fbd5c6631d7ecee19109070b34da3a86a
datastoreking <vladislavcukanovfree@gmail.com>
2022-06-19 19:58:55 +0300

    react-tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
                 <a className='flex items-center mb-4 md:mb-0 p-3 w-48 h-48 ' href="http://google.com">
                     <img src={Logo} alt="logo" className='rounded-full'/>
                 </a>
-                <span className="ml-3 text-xl font-nunito">Welcome to my First React-TailwindCss Train</span>
+                <span className="ml-3 text-xl font-nunito">Welcome to Zana</span>
                 <nav class="lg:ml-auto lg:mt-4 flex  items-center text-base justify-center">
                     <a class="mr-5 hover:text-slate-500 hover:text-2xl font-koulen" href="http://google.com">
                         Login
